923addbaa4d8cdfe220f1248f5e6a08ef1a92461
aaaaa <aaaaa@aaaa.com>
2017-04-24 01:56:04 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "toppages#index"
+  # Controller名 # アクション名 という形で表記
 end
